244ee9121e1ac6d0e732ae03422bc950fa36b273
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-05 20:34:18 -0800

    Fix deleteNode()

--- main.cpp
+++ main.cpp
@@ -119,4 +119,12 @@ void addFront(Node *&head, const int value) {
 void deleteNode(Node *&head, int position) {
     Node *current = head;
     Node *prev = head;
+    for (int i = 0; i < position - 1; ++i) {
+        if (i == 0) {
+            current = current->next;
+        } else {
+            current = current->next;
+            prev = prev->next;
+        }
+    }
 }
